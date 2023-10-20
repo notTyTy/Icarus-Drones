33097fcad502104550e5e31e90e41170e8035f47
Ash <ashy@tyty.cloud>
2023-10-20 15:43:59 +0100

    Removed a listview for a listbox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,5 +31,13 @@ namespace Icarus_Drones
         // 6.4 Create a global Queue<T> of type Drone called “ExpressService”.
         Queue<Drone> ExpressService = new();
 
+        // 6.5 Create a button method called “AddNewItem” that will add a new service item to a Queue<> based 
+        // on the priority. Use TextBoxes for the Client Name, Drone Model, Service Problem and Service Cost.
+        // Use a numeric control for the Service Tag. The new service item will be added to the appropriate
+        // Queue based on the Priority radio button.
+
+        // 6.6 Before a new service item is added to the Express Queue the service cost must be increased by 15%.
+
+
     }
 }
