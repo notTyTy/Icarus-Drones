7fa7b980a8b44890c01ea3fe6d33ec1996ef247b
Ash <ashy@tyty.cloud>
2023-10-23 10:49:51 +0100

    Added clear method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,8 +35,29 @@ namespace Icarus_Drones
         // on the priority. Use TextBoxes for the Client Name, Drone Model, Service Problem and Service Cost.
         // Use a numeric control for the Service Tag. The new service item will be added to the appropriate
         // Queue based on the Priority radio button.
+        public void AddNewItem()
+        {
+
+        }
+        // 6.17 Create a custom method that will clear all the textboxes after each service item has been added
+        public void Clearboxes()
+        {
+            ClientNameTextbox.Clear();
+            DroneModelTextbox.Clear();
+            DroneIssueTextbox.Clear();
+            ServiceTagTextbox.Clear();
+            RepairCostTextbox.Clear();
+            RegularRadio.IsChecked = false;
+            ExpressRadio.IsChecked = false;
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Clearboxes();
+        }
 
         // 6.6 Before a new service item is added to the Express Queue the service cost must be increased by 15%.
+
 
 
     }
